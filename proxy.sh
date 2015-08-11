0735bf965452db0c764eedf498c3021d98717d2d
Dewa-ubuntu <dewa_ubuntu81@yahoo.com>
2015-08-11 08:04:00 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -76,7 +76,7 @@ EOF
 
 service squid3 restart
 
-echo "Acquire::http::Proxy \"http://172.16.172.110:3128\";" | sudo tee /etc/apt/apt.conf
+echo "Acquire::http::Proxy \"http:// 128.199.159.33:3128\";" | sudo tee /etc/apt/apt.conf
 
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:openstack-ubuntu-testing/grizzly-trunk-testing
